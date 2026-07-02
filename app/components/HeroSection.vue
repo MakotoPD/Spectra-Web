46e51e7b4078f3c7b7a72dd0e56cb5f65816b94e
MakotoPD <makoto.kontakt@gmail.com>
2026-07-02 11:37:53 +0200

    Update HeroSection.vue

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -131,7 +131,7 @@ onUnmounted(() => ctx?.revert())
           <span class="inline-flex items-center gap-[7px]"><svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor"><path d="M16.4 12.6c0-2.3 1.9-3.4 2-3.5-1.1-1.6-2.8-1.8-3.4-1.8-1.4-.1-2.8.8-3.5.8s-1.8-.8-3-.8c-1.5 0-3 .9-3.8 2.3-1.6 2.8-.4 7 1.2 9.3.8 1.1 1.7 2.4 2.9 2.3 1.2 0 1.6-.7 3-.7s1.8.7 3 .7c1.2 0 2-1.1 2.8-2.2.9-1.3 1.2-2.5 1.3-2.6-.1 0-2.5-1-2.5-3.8ZM14.1 5c.6-.8 1.1-1.9 1-3-.9 0-2.1.6-2.8 1.4-.6.7-1.1 1.8-1 2.9 1 .1 2.1-.5 2.8-1.3Z" /></svg>macOS</span>
           <span class="inline-flex items-center gap-[7px]"><svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2c-1.7 0-3 1.4-3 3.2 0 1 .1 2.2-.5 3.4-.7 1.3-2 2.4-2 4.7 0 .9.3 1.7.7 2.4-.5.4-.9 1-.9 1.8 0 1.5 1.4 2.3 3.2 2.7 1 .2 1.8.9 2.5.9s1.5-.7 2.5-.9c1.8-.4 3.2-1.2 3.2-2.7 0-.8-.4-1.4-.9-1.8.4-.7.7-1.5.7-2.4 0-2.3-1.3-3.4-2-4.7-.6-1.2-.5-2.4-.5-3.4C15 3.4 13.7 2 12 2Z" /></svg>Linux</span>
           <span style="color:#3f4d63">·</span>
-          <span class="font-mono">MIT</span>
+          <span class="font-mono">GPLv3</span>
         </div>
       </div>
 
